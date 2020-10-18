c08edf401d0ca00b7cad57eea421be3a9d26cd83
eintyp <dongkingkong0@gmail.com>
2020-10-18 16:39:06 +0200

    structure & icons

--- src/index.php
+++ src/index.php
@@ -1,18 +1,74 @@
 <?php
 
+$tiles = [
+  'Vertretungsplan' => [
+    'layout' => 'vertical'
+  ],
+  'Lernraum' => [
+    'layout' => 'horizontal',
+    'href' => 'https://www.lernraum-berlin.de/v1und2/my/',
+    'icon' => 'layout'
+  ],
+  'Belegungsplan' => [
+    'href' => 'https://cis.gruener-campus-malchow.de/belegungsplan/',
+    'icon' => 'clipboard'
+  ],
+  'Timer' => [
+    'layout' => 'vertical'
+  ],
+  'Drucker/Kopierer' => [
+    'href' => 'https://gcm.schule/printer/',
+    'icon' => 'printer'
+  ],
+  'Snake' => [
+    'layout' => 'vertical'
+  ],
+  'Wetterstation' => [
+    'href' => 'http://fbi.gruener-campus-malchow.de/cis/wetterstation/',
+    'icon' => 'sun'
+  ],
+  'Webseite' => [
+    'layout' => 'vertical',
+    'href' => 'http://gruener-campus-malchow.de',
+    'subtitle' => [
+      'Heute schon gelacht?™',
+      'Traurig, aber wahr:',
+      'Die offizielle Webpräsenz unserer Schule'
+    ],
+    'icon' => 'link'
+  ],
+  'CampusTV' => [
+    'layout' => 'horizontal',
+    'href' => 'https://gcm.schule/tv/',
+    'icon' => 'tv'
+  ],
+  'Wififilter' => [
+    'href' => 'https://gcm.schule/wififilter/',
+    'icon' => 'wifi'
+  ]
+];
+
+function icon ($name) {
+  $src = 'src/icons/' . $name . '.svg';
+  if (file_exists($src)) {
+    include $src;
+  }
+}
+
 ?><!doctype html>
 <html>
   <head>
     <meta charset=utf-8>
     <meta name=viewport content="initial-scale=1">
     <title>Campus Informations System</title>
+    <link rel="stylesheet" href="build/index.css">
   </head>
   <body>
     <div class=cis-header>
       <div class=cis-brand>CIS home</div>
       <form class=cis-header-search action="https://ecosia.org/search" method=GET>
         <input class=cis-search-input type=text name=q placeholder="Umweltfreundlich Suchen" required autofocus autocomplete=off>
-        <button class=cis-search-button type=submit><svg viewBox="-2 -2 20 20"><path d="M6,0A6,6 0 01 6,12 6,6 0 01 6,0zM10.25,10.25L16,16" fill="none" stroke="#ddd" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
+        <button class=cis-search-button type=submit><?php icon('search'); ?></button>
       </form>
       <div class=spacer><!-- this is here to center the search bar. feel free to replace with something useful if you want to have something on the right --></div>
       <!--
@@ -24,7 +80,7 @@
       -->
     </div>
     <div class=grid-container>
-      <div class="grid-cell grid-cell-vertical" data-cell-action=vertretungsplan>Vertretungsplan</div>
+      <!--div class="grid-cell grid-cell-vertical" data-cell-action=vertretungsplan>Vertretungsplan</div>
       <a class="grid-cell grid-cell-horizontal" href="https://www.lernraum-berlin.de/v1und2/my/">Lernraum</a>
       <a class=grid-cell href="https://cis.gruener-campus-malchow.de/belegungsplan/">Belegungsplan</a>
       <div class="grid-cell grid-cell-vertical" data-cell-action=timer>Timer</div>
@@ -33,7 +89,30 @@
       <a class=grid-cell href="http://fbi.gruener-campus-malchow.de/cis/wetterstation/">Wetterstation</a>
       <a class="grid-cell grid-cell-vertical" href="http://gruener-campus-malchow.de">Webseite<div class="subtitle">Heute schon gelacht?™<br>Traurig, aber wahr:<br>Die offizielle Webpräsenz unserer Schule</div></a>
       <a class="grid-cell grid-cell-horizontal" href="https://gcm.schule/tv/">CampusTV</a>
-      <a class=grid-cell href="https://gcm.schule/wififilter/">Wififilter</a>
+      <a class=grid-cell href="https://gcm.schule/wififilter/">Wififilter</a-->
+
+      <?php
+      foreach ($tiles as $name => $data) {
+        // start tag
+        echo isset($data['href']) ? '<a href="' . $data['href'] . '"' : '<div';
+        echo isset($data['layout']) ? ' class="grid-cell grid-cell-' . $data['layout'] . '">' : ' class="grid-cell">';
+
+        // icon
+        if (isset($data['icon'])) {
+          icon($data['icon']);
+        }
+
+        // title
+        echo $name;
+        // subtitle
+        echo isset($data['subtitle']) ? '<div class="subtitle">' .
+          (is_array($data['subtitle']) ? implode('<br>', $data['subtitle']) : $data['subtitle'])
+          . '</div>' : '';
+
+        // end tag
+        echo isset($data['href']) ? '</a>' : '</div>';
+      }
+      ?>
     </div>
 
   </body>
