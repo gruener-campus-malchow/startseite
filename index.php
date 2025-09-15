582ee05909702edad9b10dc6f348613088206c89
ijontychie <baldauf.a@berlin.gcm.schule>
2025-09-15 12:03:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $tiles = [
 	'Pixelmap' => [
 		/*'layout' => 'vertical',*/
 		'href' => 'https://gcm.schule/pixelmap',
-		'icon' => 'pixel',
+		'icon' => 'pixels',
 	],
 	'Material' => [
 		'href' => 'https://gcm.schule/material/',
